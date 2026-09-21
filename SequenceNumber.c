88a2ca7cc429fe039c6a5e95a4b0f256f225cff7
Vaiibhavi_Singh <vaiibhavisingh855@gmail.com>
2026-09-21 15:51:16 +0530

    Added Sequence number where we n eed to find the n numbers two digit numbes when summed up gives N , using loops

---
+++ SequenceNumber.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void printSums(int N)
+{
+    int n;
+    for(n = 1; n <= N / 2; n++)
+    {
+        int sum = 0;
+        int i;
+        for(i = n; i <= N; i++)
+        {
+            sum = sum + i;
+            if(sum == N)
+            {
+                int j;
+                for(j = n; j <= i; j++)
+                {
+                    printf("%d ", j);
+                }
+                printf("\n");
+                break;
+            }
+            if(sum > N)
+            {
+                break;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    printSums(N);
+    return 0;
+}
